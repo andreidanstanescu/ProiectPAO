7a2e13dccc5f29399f03ea0cc4f7fc1a9ea37f22
andreidanstanescu <andreidanst@gmail.com>
2021-04-09 18:17:42 +0300

    finalizat etapa 1

---
+++ src/biblioteca/Person.java
@@ -0,0 +1,29 @@
+package biblioteca;
+
+public class Person {
+    private String nume,prenume;
+
+    public Person(){
+
+    }
+
+    public Person(String nume, String prenume){
+        this.nume = nume;
+        this.prenume = prenume;
+    }
+
+    public String getNume() {
+        return this.nume;
+    }
+
+    public String getPrenume() {
+        return this.prenume;
+    }
+
+    @Override
+    public String toString() {
+        return  "Nume " + nume +
+                "prenume " + prenume;
+    }
+
+}
